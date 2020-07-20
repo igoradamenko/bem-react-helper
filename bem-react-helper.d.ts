9f6501906aeb642e7a968a7cd71f71913ee6ed78
Mikhail Donskoy <m.donsk@yandex.ru>
2020-07-20 11:19:35 +0300

    Improve type for default mods.

--- bem-react-helper.d.ts
+++ bem-react-helper.d.ts
@@ -1,12 +1,8 @@
 declare module 'bem-react-helper' {
   type ModValue = string | boolean;
 
-  export type BemProps = {
-    mods?: Mods,
-    mix?: Mix,
-  };
   export type Mix = string | string[];
   export type Mods = Partial<Record<string, ModValue>>;
 
-  export default function b(name: string, props: BemProps, defaultMods?: Mods): string;
+  export default function b<T extends Mods>(name: string, props: { mods?: T, mix?: Mix }, defaultMods?: T): string;
 }
